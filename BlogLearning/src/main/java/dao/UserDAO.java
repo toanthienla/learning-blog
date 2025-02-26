1e12cc527dab4ab2b35bf2882b505f72db0ac9dd
danglnh07 <danglnh07.work@gmail.com>
2025-02-26 22:16:47 +0700

    Update to UserDAO class
    Change method create account:
     + Add email and username validation: email/username already registered
       cannot be added
     + Change from try/catch to throws Exception for better control and
       displaying in UI
     + Throw SQL Exception instead of return null if SQL failed to Insert
       new user -> Prevent unneccesary Null Pointer Exception
    
    Change login method:
     + Change from try/catch to throws Exception for better control and UI
       displaying

--- BlogLearning/src/main/java/dao/UserDAO.java
+++ BlogLearning/src/main/java/dao/UserDAO.java
@@ -4,13 +4,12 @@
  */
 package dao;
 
+import jakarta.mail.MessagingException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import model.Role;
 import model.User;
 import util.DBContext;
@@ -23,67 +22,78 @@ import util.Util;
  */
 public class UserDAO extends DBContext {
 
-    public User createAccount(String username, String email, String password, Role role) {
-        try {
-            //Get maxID first
-            String sql = "SELECT max(UserId) AS maxID FROM Users";
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            int maxID = 0;
-            if (rs.next()) {
-                System.out.println("Current max ID: " + rs.getInt("maxID"));
-                maxID = rs.getInt("maxID") + 1;
+    public User createAccount(String username, String email, String password, Role role) throws SQLException, MessagingException, IllegalArgumentException {
+        //Sanitize email (email is case insesitive)
+        email = email.toLowerCase();
+
+        //Check if the current email or username has been registered
+        String sql = "SELECT Email, UserName FROM Users WHERE Email = ? AND UserName = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, email);
+        ps.setString(2, username);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            String dbEmail = rs.getString("Email");
+            String dbUsername = rs.getString("UserName");
+            if (dbEmail != null && !dbEmail.isEmpty()) {
+                throw new IllegalArgumentException("This email has been registered!");
             }
 
-            //Insert new data to Users table
-            sql = "INSERT INTO Users (UserId, UserName, Email, Password, Role) "
-                    + "VALUES (?, ?, ?, ?, ?)";
-            ps = conn.prepareStatement(sql);
-            ps.setInt(1, maxID);
-            ps.setString(2, username);
-            ps.setString(3, email);
-            ps.setString(4, Util.hash(password));
-            ps.setInt(5, role == Role.PUBLISHER ? 0 : 1);
-            int rowAffected = ps.executeUpdate();
-            if (rowAffected > 0) {
-                //Send email
-                MailService mailService = new MailService();
-                List<String> to = new ArrayList<>();
-                to.add(email);
-                mailService.send(to, "Welcome to Blog Learning System", "welcome.html");
-                
-                //return the newly created user
-                return new User(maxID, username, email, Util.hash(password), role);
-            }            
-        } catch (SQLException e) {
-
-        } catch (Exception ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+            if (dbUsername != null && !dbUsername.isEmpty()) {
+                throw new IllegalArgumentException("This username has been used!");
+            }
         }
 
-        return null;
+        //Get maxID first
+        sql = "SELECT max(UserId) AS maxID FROM Users";
+        ps = conn.prepareStatement(sql);
+        rs = ps.executeQuery();
+        int maxID = 0;
+        if (rs.next()) {
+            System.out.println("Current max ID: " + rs.getInt("maxID"));
+            maxID = rs.getInt("maxID") + 1;
+        }
+
+        //Insert new data to Users table
+        sql = "INSERT INTO Users (UserId, UserName, Email, Password, Role) "
+                + "VALUES (?, ?, ?, ?, ?)";
+        ps = conn.prepareStatement(sql);
+        ps.setInt(1, maxID);
+        ps.setString(2, username);
+        ps.setString(3, email);
+        ps.setString(4, Util.hash(password));
+        ps.setInt(5, role == Role.PUBLISHER ? 0 : 1);
+        int rowAffected = ps.executeUpdate();
+        if (rowAffected > 0) {
+            //Send email
+            MailService mailService = new MailService();
+            List<String> to = new ArrayList<>();
+            to.add(email);
+            mailService.send(to, "Welcome to Blog Learning System", "welcome.html");
+
+            //return the newly created user
+            return new User(maxID, username, email, Util.hash(password), role);
+        }
+
+        throw new SQLException("Failed to create new account");
     }
-   
-    public User login(String email, String password) {
-        try {
-            String sql = "SELECT * FROM Users WHERE email = ? AND password = ?";
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, email);
-            ps.setString(2, Util.hash(password));
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                return new User(
-                        rs.getInt("UserId"),
-                        rs.getString("UserName"),
-                        rs.getString("email"),
-                        rs.getString("password"),
-                        rs.getInt("role") == 0 ? Role.PUBLISHER : Role.READER,
-                        rs.getInt("Point")
-                );
-            }
-        } catch (SQLException e) {
-        }
 
+    public User login(String email, String password) throws SQLException  {
+        String sql = "SELECT * FROM Users WHERE email = ? AND password = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, email);
+        ps.setString(2, Util.hash(password));
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+            return new User(
+                    rs.getInt("UserId"),
+                    rs.getString("UserName"),
+                    rs.getString("email"),
+                    rs.getString("password"),
+                    rs.getInt("role") == 0 ? Role.PUBLISHER : Role.READER,
+                    rs.getInt("Point")
+            );
+        }
         return null;
     }
 
