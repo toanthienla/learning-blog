3de870369de2e106bc5254d2fc541b2e0e59aed1
danglnh07 <danglnh07.work@gmail.com>
2025-02-27 09:17:54 +0700

    Add sign in servlet with sign in function

---
+++ BlogLearning/src/main/java/controller/SignInServlet.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller;
+
+import dao.UserDAO;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.sql.SQLException;
+import model.User;
+
+/**
+ *
+ * @author Asus
+ */
+@WebServlet(name="SignInServlet", urlPatterns={"/sign-in"})
+public class SignInServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("signIn.jsp").forward(request, response);
+    } 
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Get session from request
+        HttpSession session = request.getSession();
+        try {
+            //Get request parameter
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+            
+            //Login
+            UserDAO dao = new UserDAO();
+            User user = dao.login(email, password);
+            
+            //Add attribute to session
+            session.setAttribute("account", user);
+            //request.getRequestDispatcher("main").forward(request, response);
+            response.sendRedirect("main");
+        } catch (SQLException e) {
+            session.setAttribute("errMsg", "Internal server error!");
+            response.sendRedirect("signIn.jsp");
+        } catch (IllegalArgumentException e) {
+            session.setAttribute("errMsg", e.getMessage());
+            response.sendRedirect("signIn.jsp");
+        }
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
