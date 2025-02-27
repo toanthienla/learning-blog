df6fbd870df6092eb89c82ff07a956c0d111863d
danglnh07 <danglnh07.work@gmail.com>
2025-02-27 08:09:42 +0700

    Add new method logError
    Method to log error into [ROOT_DIRECTORY]/logs/error.log

--- BlogLearning/src/main/java/util/Util.java
+++ BlogLearning/src/main/java/util/Util.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -31,6 +32,26 @@ public class Util {
     public static String hash(String str) {
         //Reason we don't use MessageDigest class is that it's not thread safe
         return Hashing.sha256().hashString(str, StandardCharsets.UTF_8).toString();
+    }
+
+    /**
+     * Helper method to log error into [ROOT_DIRECTORY]/logs/error.log.
+     *
+     * @param message - String message
+     */
+    public static void logError(String message) {
+        try {
+            File file = new File("logs/error.log"); // Write to a "logs" folder in the project root
+            // Ensure directory exists
+            file.getParentFile().mkdirs();
+
+            //Create file writer 
+            FileWriter writer;
+            writer = new FileWriter(file, true); //true means the message will be appended instead of rewrite
+            writer.write(message + System.lineSeparator());
+            writer.close();
+        } catch (IOException ex) {
+        }
     }
 
     /**
@@ -74,5 +95,6 @@ public class Util {
 
     public static void main(String[] args) {
         //getResource();
+        //logError("Hello world");
     }
 }
